f24020c17c84a1b3462bc13106ed51ca9d97e308
Alyetama <f.catus@pm.me>
2021-03-19 14:10:41 -0500

    update

--- compile_model.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-module load anaconda
-module load compiler/gcc/4.7
-module load cuda
-$WORK/.conda/envs/ada-env/bin/python $WORK/ADA_Project/StyleGAN2-ada__source_code/train.py --outdir=/work/chaselab/malyetama/ADA_Project/training_runs/metfaces_training-runs --gpus=2 --data=/work/chaselab/malyetama/ADA_Project/datasets/metfaces_custom --snap=1 --kimg=1
