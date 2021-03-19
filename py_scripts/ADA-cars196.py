93d793353361e54fc50deee8810b793e2e4648a1
Alyetama <56323389+Alyetama@users.noreply.github.com>
2021-03-19 10:12:07 -0500

    update

--- py_scripts/ADA-cars196.py
+++ py_scripts/ADA-cars196.py
@@ -69,7 +69,7 @@ print(latest_snap)
 
 run_desc, training_options = DeepFAMS.setup_training_options(
     gpus       = 2,
-    snap       = 1,
+    snap       = 30,
     data       = DATA_CUSTOM_DIR,
     resume     = latest_snap
 )
