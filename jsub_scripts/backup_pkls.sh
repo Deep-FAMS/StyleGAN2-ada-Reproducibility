787cbd303f1c4dc3f0118de0f0e894cbf03e99cc
Alyetama <f.catus@pm.me>
2021-03-21 13:12:10 -0500

    update

--- jsub_scripts/backup_pkls.sh
+++ jsub_scripts/backup_pkls.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 #SBATCH --time=72:00:00
-#SBATCH --job-name='ADA-gen1_pokemon'
-#SBATCH --partition='gpu'
-#SBATCH --gres=gpu:2
-#SBATCH --constraint=gpu_32gb&gpu_v100
-#SBATCH --mem=64gb
+#SBATCH --job-name='backup_pkls'
+#SBATCH --mem=2gb
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user='en4byxffy93v6uy@pipedream.net'
 #SBATCH --error='/work/chaselab/malyetama/ADA_Project/jobs_log/%J.err'
 #SBATCH --output='/work/chaselab/malyetama/ADA_Project/jobs_log/%J.out'
 
-cd $WORK/ADA_Project
+cd $WORK
+module load rclone
 module load cuda
 module load anaconda
 module load compiler/gcc/6.1
-conda activate $WORK/malyetama/.conda/envs/ada-env
-$WORK/.conda/envs/ada-env/bin/python $WORK/ADA_Project/py_scripts/ADA-gen1_pokemon.py
+conda activate $WORK/.conda/envs/ada-env
+while true; do $WORK/.conda/envs/ada-env/bin/python  python $WORK/ADA_Project/backup_pkls.py; sleep 3600; done
