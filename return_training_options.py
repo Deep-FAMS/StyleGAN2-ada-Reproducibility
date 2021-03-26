d56cfed52242ffc54da6420f52626cb2cfb3f5fd
Alyetama <56323389+Alyetama@users.noreply.github.com>
2021-03-26 14:06:54 -0500

    update

---
+++ return_training_options.py
@@ -0,0 +1,36 @@
+import os
+from glob import glob
+import re
+import shutil
+from pathlib import Path
+
+
+def return_training_options(PROJ_DIR=f'{os.environ["WORK"]}/ADA_Project'):
+    
+os.chdir(PROJ_DIR)
+
+TRfolders = f'{PROJ_DIR}/training_runs/'
+TRfolders_ = glob(f'{PROJ_DIR}/training_runs/*')
+datasets = [x.replace(TRfolders, '').replace('_training-runs', '') for x in TRfolders_]
+datasets = ['AFHQ-CAT' if x == 'AFHQ' else x for x in datasets]
+
+d = {}
+
+for folder, dataset in zip(TRfolders_, datasets):
+    files = sorted(glob(folder + "/**/*"))
+    files = [x for x in files if 'training_options' in x and 'pokemon' not in x]
+    if files == []:
+        continue
+    else:
+        d[dataset] = {}
+        d[dataset]['file'] = files[-1]
+
+
+for k, v in d.items():
+    file = v['file']
+    file_dst = f'{PROJ_DIR}/training_options/{Path(file).parent.name.replace("AFHQ_custom", "AFHQ-CAT_custom")}__{Path(file).name}'
+    shutil.copyfile(file, file_dst)
+    print(f' Copying: {file} to ==> {file_dst}\n')
+
+
+return_training_options()
