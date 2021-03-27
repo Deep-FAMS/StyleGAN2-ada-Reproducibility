6fa9de0469d034bdc1a64f4f53bae5999a590ea8
Alyetama <56323389+Alyetama@users.noreply.github.com>
2021-03-27 15:16:39 -0400

    update

--- jsub_scripts/rclone_backup.sh
+++ jsub_scripts/rclone_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=08:00:00
+#SBATCH --time=72:00:00
 #SBATCH --job-name='rclone'
-#SBATCH --mem=16gb
+#SBATCH --mem=4gb
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user='en4byxffy93v6uy@pipedream.net'
 #SBATCH --error='/work/chaselab/malyetama/ADA_Project/jobs_log/%J.err'
@@ -9,5 +9,8 @@
 
 cd $WORK
 module load rclone
-rclone sync /work/chaselab/malyetama/ADA_Project GoogleDrive:/ADA_Project_backup -P
-
+module load cuda
+module load anaconda
+module load compiler/gcc/6.1
+conda activate /work/chaselab/malyetama/.conda/envs/ada-env
+while true; do /work/chaselab/malyetama/.conda/envs/ada-env/bin/python /work/chaselab/malyetama/ADA_Project/crane_scripts/backup_pkls.py; sleep 3600; done
