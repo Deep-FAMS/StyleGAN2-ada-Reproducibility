ef36d570c7cfe3269929e0ad08bba2cb9a8d8cb0
Alyetama <56323389+Alyetama@users.noreply.github.com>
2021-03-27 15:16:38 -0400

    fixed bugs

--- jsub_scripts/ADA-cars196.sh
+++ jsub_scripts/ADA-cars196.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
-#SBATCH --job-name='ADA-102flowers'
+#SBATCH --job-name='ADA-cars196'
 #SBATCH --partition='gpu'
 #SBATCH --gres=gpu:2
 #SBATCH --constraint=gpu_32gb&gpu_v100
@@ -10,9 +10,8 @@
 #SBATCH --error='/work/chaselab/malyetama/ADA_Project/jobs_log/%J.err'
 #SBATCH --output='/work/chaselab/malyetama/ADA_Project/jobs_log/%J.out'
 
-cd $WORK/ADA_Project
 module load cuda
 module load anaconda
 module load compiler/gcc/6.1
 conda activate /work/chaselab/malyetama/.conda/envs/ada-env
-/work/chaselab/malyetama/.conda/envs/ada-env/bin/python /work/chaselab/malyetama/ADA_Project/py_scripts/ADA-102flowers.py
+/work/chaselab/malyetama/.conda/envs/ada-env/bin/python /work/chaselab/malyetama/ADA_Project/py_scripts/ADA-cars196.py
